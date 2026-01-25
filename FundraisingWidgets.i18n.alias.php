f5f58bcb37a8e94ea8f73ec79d160b76bb3df4ea
Jack Gleeson <jgleeson@wikimedia.org>
2026-01-24 19:01:23 -0500

    Add missing alias config

---
+++ FundraisingWidgets.i18n.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for special pages of the FundraisingWidgets extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'FundraisingWidgets' => [ 'FundraisingWidgets', 'Fundraising Widgets' ],
+];
